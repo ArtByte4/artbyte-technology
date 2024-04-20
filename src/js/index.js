3b06389356ddebd2da7d086215a9222b1ad24780
ArtByte4 <161776090+ArtByte4@users.noreply.github.com>
2024-04-19 20:10:24 -0500

    Mejora de version responsive
    Mejora resposive.
    Se añadio el speech

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,16 @@
 
+let load = document.getElementById('load')
 
+window.addEventListener('load', () => {
+   setTimeout(() => {
+    load.style.display = 'none'
+
+   }, 2000)
+  
+})
+let menu = document.getElementById('menu');
+let menu_movile = document.getElementById('menu_movil')
+
+menu.addEventListener('click', () => {
+    menu_movile.style.display == 'flex' ? menu_movile.style.display = 'none' :menu_movile.style.display= 'flex'
+})
